8eb54ff09ae1cd9063f6b109cc81d2c85b1f9070
Henrriky <tectutors.123@gmail.com>
2023-07-14 17:18:49 -0300

    Send prato form

--- src/paginas/Administracao/Pratos/FormularioPrato.tsx
+++ src/paginas/Administracao/Pratos/FormularioPrato.tsx
@@ -1,63 +1,122 @@
-import { AppBar, Box, Button, Container, Link, Paper, TextField, Toolbar, Typography } from "@mui/material";
-import axios from "axios";
+import { Box, Button, FormControl, InputLabel, MenuItem, Select, TextField, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
-import { useParams, Link as RouterLink } from "react-router-dom";
-import IRestaurante from "../../../interfaces/IRestaurante";
+import ITag from "../../../interfaces/ITag";
 import http from "../../../http";
-import AppBarLayout from "../../../componentes/PaginaBaseAdmin/AppBarLayout";
+import IRestaurante from "../../../interfaces/IRestaurante";
 
 export default function FormularioPrato() {
 
-    const parametros = useParams();
+    const [nomePrato, setNomePrato] = useState('');
+    const [descricao, setDescricao] = useState('');
+
+    const [tag, setTag] = useState('');
+    const [tags, setTags] = useState<ITag[]>([]);
+
+    const [restaurante, setRestaurante] = useState('');
+    const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
+
+    const [imagem, setImagem] = useState<File | null>(null);
 
     useEffect(() => {
-        if (parametros.id) {
-            http.get<IRestaurante>(`restaurantes/${parametros.id}/`)
-                .then(resposta => setNomeRestaurante(resposta.data.nome))
+        http.get<{ tags: Array<ITag> }>('tags/')
+            .then(resposta => setTags(resposta.data.tags))
+        http.get<IRestaurante[]>('restaurantes/')
+            .then(resposta => setRestaurantes(resposta.data))
+    }, [])
+
+    const selecionarArquivo = (evento: React.ChangeEvent<HTMLInputElement>) => {
+        if (evento.target.files?.length) {
+            setImagem(evento.target.files[0]);
+        } else {
+            setImagem(null);
         }
-    }, [parametros])
-
-    const [nomeRestaurante, setNomeRestaurante] = useState('');
-
+    }
     const aoSubmeterFormulario = (evento: React.FormEvent<HTMLFormElement>) => {
         evento.preventDefault();
 
-        if (parametros.id) {
+        const formData = new FormData();
+        formData.append('nome', nomePrato);
+        formData.append('descricao', descricao);
+        formData.append('tag', tag);
+        formData.append('restaurante', restaurante);
 
-            http.put(`restaurantes/${parametros.id}/`, {
-                nome: nomeRestaurante,
-            }).then(() => {
-                alert("Restaurante atualizado com sucesso!")
-            })
-
-        } else {
-            http.post('restaurantes/', {
-                nome: nomeRestaurante
-            })
-                .then(() => {
-                    alert("Restaurante cadastrado com sucesso!")
-                })
+        if (imagem) {
+            formData.append('imagem', imagem);
         }
+
+        http.request({
+            url: 'pratos/',
+            method: 'POST',
+            headers: {
+                'Content-type': 'multipart/form-data'
+            },
+            data: formData
+        }).then(() => {
+            setNomePrato('')
+            setTag('')
+            setDescricao('')
+            setRestaurante('')
+            setImagem(null)
+            alert('Prato cadastrado com sucesso')
+        }
+        )
+            .catch(error => console.log(error));
+
+
+
     }
+
+
 
     return (
         <Box sx={{ display: 'flex', flexDirection: "column", alignItems: "center", flexGrow: 1 }}>
             <Typography component="h1" variant="h6">
-                {
+                {/* {
                     parametros.id === undefined ?
-                        'Formulário para criar restaurante' :
-                        'Formulário para editar restaurante'
-                }
+                        'Formulário para criar prato' :
+                        'Formulário para editar prato'
+                } */}
             </Typography>
             <Box onSubmit={aoSubmeterFormulario} component="form" sx={{ width: '100%' }} >
                 <TextField
-                    label="Nome do restaurante"
+                    label="Nome do prato"
                     variant="standard"
-                    onChange={evento => setNomeRestaurante(evento.target.value)}
-                    value={nomeRestaurante}
+                    onChange={evento => setNomePrato(evento.target.value)}
+                    value={nomePrato}
                     fullWidth
                     required
+                    margin="dense"
                 />
+                <TextField
+                    label="Descrição do prato"
+                    variant="standard"
+                    onChange={evento => setDescricao(evento.target.value)}
+                    value={descricao}
+                    fullWidth
+                    required
+                    margin="dense"
+                />
+                <FormControl margin="dense" fullWidth>
+                    <InputLabel id="select-tag">Tag</InputLabel>
+                    <Select labelId="select-tag" value={tag} onChange={evento => setTag(evento.target.value)}>
+                        {tags.map(tag => (
+                            <MenuItem value={tag.value} key={tag.id}>{tag.value}</MenuItem>
+                        )
+                        )
+                        }
+                    </Select>
+                </FormControl>
+                <FormControl margin="dense" fullWidth>
+                    <InputLabel id="select-tag">Restaurante</InputLabel>
+                    <Select labelId="select-tag" value={restaurante} onChange={evento => setRestaurante(evento.target.value)}>
+                        {restaurantes.map(restaurante => (
+                            <MenuItem value={restaurante.id} key={restaurante.id}>{restaurante.nome}</MenuItem>
+                        )
+                        )
+                        }
+                    </Select>
+                </FormControl>
+                <input type="file" onChange={selecionarArquivo} />
                 <Button sx={{ marginTop: 1 }} type="submit" variant="outlined" fullWidth>Salvar</Button>
             </Box>
         </Box>
